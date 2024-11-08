acf49bd1d66d8bbfec7f2014a0233080ee721430
Jatan Dudeja <dudeja.jatan04@gmail.com>
2024-11-09 02:31:21 +0530

    Solved Longest Repeating Character Replacement. - Best Approach.

--- LeetCode/Sliding Window/Medium/longestRepeatingCharacterReplacement.cpp
+++ LeetCode/Sliding Window/Medium/longestRepeatingCharacterReplacement.cpp
@@ -55,3 +55,53 @@ public:
         return maxLen;
     }
 };
+
+
+
+// Best Solution
+// T.C. -> O(N)
+// S.C. -> O(26)
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> arr(26);
+
+        int i = 0;
+        int j = 0;
+
+        int n = s.size();
+
+        int maxFrequency = 0;
+        int maxLen = INT_MIN;
+
+        while (j < n) {
+            // increase frequency of current character
+            arr[s[j] - 'A']++;
+            // check if the frequency of current character is max or not
+            maxFrequency = max(maxFrequency, arr[s[j] - 'A']);
+            // cout << i << ", " << j << ", " << s[j] << ", " << arr[s[j] - 'A']
+            // << ", " << maxFrequency << endl;
+
+            // if at any point maxFrequency is greater than k this means we
+            // cannot calculate max len of the subarray as we only need at most
+            // k operations to be done in condition we just move i to next char
+            // and also decrease the frequency of the char at ith position
+            if (((j - i + 1) - maxFrequency) > k) {
+                arr[s[i] - 'A']--;
+                i++;
+            }
+
+            // if length of subarray - maxFrequency <= k which
+            // means this condition is valid and we can calculate max length now
+            if (((j - i + 1) - maxFrequency) <= k) {
+                // cout << "in: " << i << ", " << j << ", " << s[j] << ", " <<
+                // arr[s[j] - 'A'] << ", " << maxFrequency << endl;
+                maxLen = max(maxLen, j - i + 1);
+                j++;
+            }
+        }
+
+        return maxLen;
+    }
+};
